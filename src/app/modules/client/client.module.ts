dbe0ec261e9356ec12584232e7ce1bfe660f57e1
tomas.blaha.lib@gmail.com <tomas.blaha.lib@gmail.com>
2019-01-07 16:24:40 +0100

    Dashboard

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import {CountoModule} from 'angular2-counto';
 
 @NgModule({
   imports: [
     CommonModule,
+    CountoModule,
     ClientRoutingModule
   ],
   declarations: [DashboardComponent]
