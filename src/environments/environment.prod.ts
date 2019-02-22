4baeb1b1bd469869b1c6c7bb34277adac786da2e
Tomas Blaha <OLH@HUD.cz>
2019-02-22 14:12:04 +0100

    Ssl wrong url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://payvont.com:8765',
+  API_URL: 'http://payvont.com:8765',
   grantType: 'password',
   clientId: 'test',
   clientSecret: 'secret',
